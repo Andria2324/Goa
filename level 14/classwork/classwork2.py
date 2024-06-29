2289a0fe03cdd901376c92303195ce9d5eacea16
Andria2324 <tinatinshavdia@yahoo.com>
2024-06-29 13:11:32 +0400

    commit

---
+++ level 14/classwork/classwork2.py
@@ -0,0 +1,10 @@
+print(False and True)
+print(True and False)
+print(False and true and True and False)
+
+
+print(True or False)
+print (False or True)
+print(False or True or True or False)
+print(False or True or True and False)
+print(True or False or False and True)
