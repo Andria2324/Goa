6fe1dcb6e0155a18b6de40fc7268dbb87ad0595d
Andria2324 <tinatinshavdia@yahoo.com>
2024-07-13 17:43:14 +0400

    commit

---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,29 @@
+num1=11.4
+num2=12.5
+print(num1+num2)
+
+a="Basket"
+b="ball"
+print(a+b)
+
+name="andria"
+age=12
+print(age+10)
+
+num1=int(input("enter your number:"))
+num2=int(input( "enter your number:"))
+print(num1+num2)
+
+
+
+print(2345>1234)
+print(2020<2021)
+print(2030>2028)
+print(23>21)
+print(2030<2031)
+print(4040>4030)
+print(3030>3020)
+print(7070>7060)
+print(2002==2002)
+print(3030>80)
+print(33>22)
