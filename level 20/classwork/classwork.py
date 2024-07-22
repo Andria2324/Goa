dfe170855570fee337d6ac1aae963d90c1b5b052
Andria2324 <tinatinshavdia@yahoo.com>
2024-07-22 20:53:25 +0400

    commit

---
+++ level 20/classwork/classwork.py
@@ -0,0 +1,5 @@
+age=16
+if age >= 18:
+   print("you are an adult")
+else:
+    print("you are not the adult yet")
---
+++ level 20/homework/homework.py
@@ -0,0 +1,26 @@
+num=int(input("enter your number:"))
+if num==18:
+    print("you won a toy dinosaur")
+if num==33:
+    print("you won a toy rabbit")
+else:
+    print("try again later")    
+
+
+age=int(input("enter your age:"))
+if age<13:
+    print("you are a kid")
+if age>13:
+    print("you are a teenager")    
+if age >20:
+    print("you are grown up")  
+
+
+
+for i in range(20):
+   num2=0
+if num2 <=10:
+    print("hello world")
+else:
+    print("goodbye world")
+
