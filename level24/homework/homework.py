03b13134367aa2956380ae996ae98a2048994782
Andria2324 <tinatinshavdia@yahoo.com>
2024-08-10 20:04:13 +0400

    commit

---
+++ level24/homework/homework.py
@@ -0,0 +1,8 @@
+listn=[1,2.0,3,True,5,0,6,8.0,7,"string"]
+#      0 1 2 3  4   5 6 7  8  9   10
+print(listn[2])
+print(listn[0:2:4])
+print(listn[7:8:9])
+print(listn[-1:-2:-3])
+print(listn[-0:-1:-2:-3])
+print(listn[-1:-2:-3])
