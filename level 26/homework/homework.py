33cebf116d5679dfdb38a9bd812fd857c0601fd1
Andria2324 <tinatinshavdia@yahoo.com>
2024-08-17 17:48:56 +0400

    commit

---
+++ level 26/homework/homework.py
@@ -0,0 +1,30 @@
+def addition():
+    i=1
+    print(i+1)
+
+def multiplication():
+    i=1
+    a=2
+    s=4
+    print(1/2/4)  
+
+
+def greeting(name,surname):
+    name="Andria"
+    surname="Bezhanishvili"
+    print("hello"+name+surname) 
+
+i(num1,num2):
+      print(num1+num2)
+
+       
+
+i(490,70)
+
+
+i(num1,num2,num3):
+        print(num1/num2/num3)
+
+i(1,2,3)
+
+           
