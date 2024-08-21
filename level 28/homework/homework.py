af24742752f776deae2c227ccee79c752d121a80
Andria2324 <tinatinshavdia@yahoo.com>
2024-08-21 17:27:09 +0400

    commit

---
+++ level 28/homework/homework.py
@@ -0,0 +1,18 @@
+name="andria"
+print(name)
+name=name.upper()
+print(name)
+
+lastname="BEZHANISHVILI"
+print(lastname)
+lastname=lastname.lower()
+print(lastname)
+
+brand="adidas"
+print(brand)
+brand=brand.capitalize()
+print(brand)
+
+club="Real madrid"
+print(club)
+club=club.swapcase()
