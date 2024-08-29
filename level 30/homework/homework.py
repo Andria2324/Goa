40a95c3aec0696d51be7f819954b7fba4cad26e1
Andria2324 <tinatinshavdia@yahoo.com>
2024-08-29 16:50:30 +0400

    commit

---
+++ level 30/homework/homework.py
@@ -0,0 +1,56 @@
+#num1=int(input("enter your number":))
+#num2=int(input("enter your number":))
+#print(num1+num2)
+
+#word1="foot"
+#word2="ball"
+#print(word1+word2)
+
+
+
+#num1=24
+#num2=2
+#print(num1/num2)
+
+200000>10000
+
+5+6==3+8
+
+
+#In Python, Logical operators are used on conditional statements (either True or False). They perform Logical AND, Logical OR, and Logical NOT operations.
+
+
+True and False and True and False and True
+False or True or False or True or False
+
+for i in range (10):
+    print(i)
+
+for i in range (10):
+    print("hello world")   
+
+
+
+s=10
+while s==10:
+    for s in range(10):
+        print(s)
+
+
+s=100
+while s==100:
+   for s in range(50,60):
+       print(s)
+
+
+
+s=50
+while s==50:
+    for s in range(1,50):
+        print(s)
+
+
+test case:[2,3,4,5,6]output:4        
+
+
+
